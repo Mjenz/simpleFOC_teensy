7910e49f8aa76263a9c0898882a05c24dae6e37a
Mjenz <michael.jenz77@gmail.com>
2026-03-17 12:48:30 -0500

    Working step detection algorithm

--- src/imu_test.cpp
+++ src/imu_test.cpp
@@ -1,23 +1,24 @@
 #include "lsm6dsv.hpp"
+#include "step_detection.hpp"
 #include <TeensyTimerTool.h>
 
 TeensyTimerTool::PeriodicTimer imu_timer_(TeensyTimerTool::TCK);
-TeensyTimerTool::PeriodicTimer step_detect_timer_(TeensyTimerTool::TCK);
 TeensyTimerTool::PeriodicTimer print_timer_(TeensyTimerTool::TCK);
 
 LSM6DSV_IMU imu;
 
+StepDetector step_detector(15, 1.336, 0.2, 1000);
+
 static accelerations imu_data;
+static bool step;
+static double cadence;
 
 void imu_loop()
 {
   imu_data = imu.get_acc();
+  step = step_detector.filter_update(imu_data);
+  cadence = step_detector.get_cadence();
 }
-
-// void step_detect_loop()
-// {
-  
-// }
 
 void print_loop()
 {
@@ -29,6 +30,13 @@ void print_loop()
 
   Serial.print(">az:");
   Serial.println(imu_data.z, 3);
+
+  Serial.print(">cadence:");
+  Serial.println(cadence);
+
+  Serial.print(">step:");
+  Serial.println(int(step));
+
 }
 void setup() 
 {
@@ -42,13 +50,13 @@ void setup()
   imu_timer_.begin(
     [](){
       imu_loop();
-    }, 1000);
+    }, 1ms);
 
   // start print loop
   print_timer_.begin(
     [](){
       print_loop();
-    }, 10000);
+    }, 10ms);
 }
 
 void loop() 
