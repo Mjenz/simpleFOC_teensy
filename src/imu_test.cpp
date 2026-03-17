9119a593db46c93e5b5b9e331deea4ac6b9bb34e
Mjenz <michael.jenz77@gmail.com>
2026-03-17 01:13:58 -0500

    Started test code for imu testing. It is lacking step detection code

--- src/imu_test.cpp
+++ src/imu_test.cpp
@@ -2,14 +2,33 @@
 #include <TeensyTimerTool.h>
 
 TeensyTimerTool::PeriodicTimer imu_timer_(TeensyTimerTool::TCK);
+TeensyTimerTool::PeriodicTimer step_detect_timer_(TeensyTimerTool::TCK);
+TeensyTimerTool::PeriodicTimer print_timer_(TeensyTimerTool::TCK);
 
 LSM6DSV_IMU imu;
 
+static accelerations imu_data;
+
 void imu_loop()
 {
-  auto imu_data = imu.get_acc();
+  imu_data = imu.get_acc();
+}
 
-  Serial.println(">> ")
+// void step_detect_loop()
+// {
+  
+// }
+
+void print_loop()
+{
+  Serial.print(">ax:");
+  Serial.println(imu_data.x, 3);
+
+  Serial.print(">ay:");
+  Serial.println(imu_data.y, 3);
+
+  Serial.print(">az:");
+  Serial.println(imu_data.z, 3);
 }
 void setup() 
 {
@@ -18,6 +37,18 @@ void setup()
 
   // init imu
   imu.init();
+
+  // start imu loop
+  imu_timer_.begin(
+    [](){
+      imu_loop();
+    }, 1000);
+
+  // start print loop
+  print_timer_.begin(
+    [](){
+      print_loop();
+    }, 10000);
 }
 
 void loop() 
